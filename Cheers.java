8367d0fa6b726656b31ebef9299b716174563c68
daniel daskal <daskal.daniel@gmail.com>
2024-11-18 21:51:51 +0200

    task comleted

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,34 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
-        }
-}
+         // inpot name as and times of cheers  
+	    String s = args[0];
+        int times = Integer.parseInt(args[1]);
+        int cheer = 0;
+
+            int x = 0;
+            int y = s.length() -1;
+            String an = "AEFHILMNORSX";
+
+            while( x <= y){ 
+            char c = s.charAt(x);
+
+              if (an.indexOf(c) == -1){
+                System.out.println("Give me" + " a  " + s.charAt(x) + ": " + s.charAt(x) + "!" );
+              }else{
+                 System.out.println("Give me" + " an " + s.charAt(x) + ": " + s.charAt(x) + "!" );}
+               
+                x++;
+                }
+                System.out.println("What does that spell?");
+                
+                while (cheer < times) {
+                    System.out.println(s + "!!!");
+                    cheer++;
+                }
+                    
+                }
+
+            }
+        
+
