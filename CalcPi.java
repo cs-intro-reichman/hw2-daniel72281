2f7018348a3c9ff8221dfb190fbe6921783fc0c3
daniel daskal <daskal.daniel@gmail.com>
2024-11-19 18:24:01 +0200

    task comleted

--- CalcPi.java
+++ CalcPi.java
@@ -2,20 +2,27 @@
 public class CalcPi {
 	public static void main(String [] args) { 
 
+    // created set 0f ints neccery for pi clac.
 	int times = Integer.parseInt(args[0]);
 	int t = 0;
-	
-	double divisor1 = 3;
-	double divisor2 = 5;
+	int divisor = 3;
 	double pi = 1;
-	while (t <= times) {
-		pi = pi - 1/divisor1 + 1/divisor2;
-		divisor1 = divisor1 + 4;
-		divisor2 = divisor2 + 4;	
+
+	//created a loop for pi clac, the loop already starts from (1-1\3) so the loop runs for the inpot numbre-1 
+	while (t < times-1) {
+		//created condition to check if +/- to value of pi(odd=+)
+		if (t%2 == 0){
+			pi = pi - (1.0/divisor);
+		}else{
+			pi = pi + (1.0/divisor);
+		}
+		// increace divisor as the loop continues
+		divisor = divisor + 2;	
 		t++;		
 	  }
 	    System.out.println("pi according to java: " + Math.PI );
-		System.out.println("pi, aprproximated:    " + pi*4 );
+		System.out.println("pi, approximated:     " + pi*4 );
+		
 	}
 }
 
