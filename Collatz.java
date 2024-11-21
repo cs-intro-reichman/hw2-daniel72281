59b3a66cb4fb02152d6d6192a524214c437dd888
daniel daskal <daskal.daniel@gmail.com>
2024-11-21 21:20:25 +0200

    added comments

--- Collatz.java
+++ Collatz.java
@@ -5,8 +5,13 @@ public class Collatz {
 		String theLetter = args[1];
 		int next = seed;
 		int count = 0;
+		
+		// compering inpot to v or else(c) to print string as needed all the code is nested inside this loop
 		String v = "v";
 		if (theLetter.equals(v)) {
+			// to overcome difficulty with seed value 1 created a condition that prints string as calc,
+			//else valeu will go inside main loop
+
 			if (seed == 1){ 
 				System.out.println(seed +" "+ (seed *3 +1) +" "+ (seed +1) +" " + seed + " (4)" );
 				System.out.println("Every one of the first " +seed +" hailstone sequences reached 1.");		
@@ -15,12 +20,14 @@ public class Collatz {
 			else{
 	
 				System.out.println("1 4 2 1 (4)");
+				// main loop start from 2 and goes up to inpot seed while counting steps
 				
 				for(int i = 2 ; i <= seed; i++) {
 					next = i;
 					count =1;
+				//nested while loop insade the main for loop to calc the sequence as needed, divided to odd or even calc
 	
-			while (next != 1) {
+			    while (next != 1) {
 				
 			
 				if (next % 2 == 0) {
@@ -36,10 +43,11 @@ public class Collatz {
 	
 				}
 			}	
-	
+	        // prints the sequence
 			System.out.println(next + " (" + count + ")" );
 			
-			}		
+			}
+					
 			System.out.println("Every one of the first " +seed +" hailstone sequences reached 1.");		
 			}
 		}
